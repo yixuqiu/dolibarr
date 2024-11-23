ecd90383054e6e325085713e6f35e1dd4e40507d
Frédéric France <frederic.france@free.fr>
2024-11-23 11:14:13 +0100

    fix phpstan doc ui

--- htdocs/admin/tools/ui/components/badges.php
+++ htdocs/admin/tools/ui/components/badges.php
@@ -3,19 +3,18 @@
  * Copyright (C) 2024 Anthony Damhet <a.damhet@progiseize.fr>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
- * This program and files/directory inner it is free software: you can
- * redistribute it and/or modify it under the terms of the
- * GNU Affero General Public License (AGPL) as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AGPL for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU AGPL
- * along with this program. If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 // Load Dolibarr environment
