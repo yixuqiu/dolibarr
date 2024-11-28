caabaabb54691d56273733999cbd1d8425b9d510
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2024-11-28 13:59:07 +0100

    Added table for handling tdoverflowmax

--- htdocs/projet/element.php
+++ htdocs/projet/element.php
@@ -1292,6 +1292,7 @@ foreach ($listofreferent as $key => $value) {
 				if ($tablename == 'expensereport_det') {
 					print $expensereport->getNomUrl(1);
 				} else {
+					print '<table><tr><td style="border-bottom: none;">';
 					// Show ref with link
 					if ($element instanceof Task) {
 						print $element->getNomUrl(1, 'withproject', 'time');
@@ -1327,6 +1328,10 @@ foreach ($listofreferent as $key => $value) {
 					}
 					print '</div>';
 
+					print '</td>';
+
+					print '<td class="tdoverflowmax250" style="border-bottom: none;">';
+
 					// Show supplier ref
 					if (!empty($element->ref_supplier)) {
 						print ' - '.$element->ref_supplier;
@@ -1339,6 +1344,8 @@ foreach ($listofreferent as $key => $value) {
 					if (empty($element->ref_customer) && !empty($element->ref_client)) {
 						print ' - '.$element->ref_client;
 					}
+
+					print '</td></tr></table>';
 				}
 				print "</td>\n";
 				// Product and qty on stock movement
