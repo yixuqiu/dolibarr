23ed96f0e24a857a6a30cf2f7b3ef399902cfd68
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-12-07 14:45:34 +0100

    Update mailing-unsubscribe.php

--- htdocs/public/emailing/mailing-unsubscribe.php
+++ htdocs/public/emailing/mailing-unsubscribe.php
@@ -56,7 +56,9 @@ if (! defined('NOREQUIREAJAX')) {
 // Load Dolibarr environment
 require '../../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
-
+/**
+ * @var DoliDB $db
+ */
 global $user, $conf, $langs;
 
 $langs->loadLangs(array("main", "mails"));
