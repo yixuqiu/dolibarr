981c13c788369df700b29e3e9229a7b0d08e1ab4
Frédéric France <frederic.france@free.fr>
2024-12-09 18:24:16 +0100

    fix phpstan

--- scripts/emailings/reset-invalid-emails.php
+++ scripts/emailings/reset-invalid-emails.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-/* Copyright (C) 2020 Laurent Destailleur <eldy@users.sourceforge.net>
+/* Copyright (C) 2020       Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
