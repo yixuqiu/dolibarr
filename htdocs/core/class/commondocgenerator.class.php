e0e559466901be1ca12184706e9535bc4653aaa2
tnegre <tnegre@open-dsi.fr>
2024-12-16 11:10:52 +0100

    add invoice type label

--- htdocs/core/class/commondocgenerator.class.php
+++ htdocs/core/class/commondocgenerator.class.php
@@ -779,6 +779,14 @@ abstract class CommonDocGenerator
 			$resarray[$array_key.'_total_discount_ht'] = '';
 		}
 
+		if ($object->element == 'facture' || $object->element == 'invoice_supplier') {
+			if ($object->type == 0) {
+				$resarray[$array_key.'_type_label'] = $outputlangs->transnoentities("PdfInvoiceTitle");
+			} else {
+				$resarray[$array_key.'_type_label'] = (empty($object)) ? '' : $object->getLibType(0);
+			}
+		}
+
 		// Fetch project information if there is a project assigned to this object
 		if ($object->element != "project" && !empty($object->fk_project) && $object->fk_project > 0) {
 			if (!is_object($object->project)) {
