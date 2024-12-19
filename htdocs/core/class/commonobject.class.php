425c23d28cafc1ada9c2090eac219f3a604320f7
Benoît PASCAL <pcl.ben.6@gmail.com>
2024-12-19 15:38:19 +0100

    fix: fetch action linked object

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -3832,7 +3832,7 @@ abstract class CommonObject
 			$num = $this->db->num_rows($resql);
 			$i = 0;
 			while ($i < $num) {
-				$obj = $this->db->fetch_object($resql);
+                $obj = $this->db->fetch_object($resql);
 				if ($justsource || $justtarget) {
 					if ($justsource) {
 						$this->linkedObjectsIds[$obj->targettype][$obj->rowid] = $obj->fk_target;
@@ -3892,8 +3892,12 @@ abstract class CommonObject
 						$classpath = 'adherents/class';
 						$module = 'adherent';
 					} elseif ($objecttype == 'contact') {
-						 $module = 'societe';
-					}
+                        $module = 'societe';
+                    } elseif ($objecttype == 'action') {
+                        $module = 'agenda';
+                        $subelement = 'actionComm';
+                    }
+
 					// Set classfile
 					$classfile = strtolower($subelement);
 					$classname = ucfirst($subelement);
