802562a575b0c0e0952dfdfe439a876ff382a2e1
Laurent Destailleur (aka Eldy) <eldy@destailleur.fr>
2025-01-09 13:54:25 +0100

    Fix regression after a fix

--- test/phpunit/test.php
+++ test/phpunit/test.php
@@ -12,18 +12,30 @@ include_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 $langs->setDefaultLang('fr');
 $langs->loadLangs(array('main', 'companies'));
 
-var_dump($langs->tab_translate["Preview"]);
+$s = '<b>aa</b> & &amp; a=%10';
+print $s."\n";
+//print dol_htmlentitiesbr($s)."\n";
+//print dol_escape_htmltag(dol_string_onlythesehtmltags(dol_htmlentitiesbr($s), 1, 0, 0, 0, array('br', 'b', 'font', 'hr', 'span')), 1, -1, '', 0, 1);
+print dolPrintHTMLForAttributeUrl('<b>aa</b> & &amp; a=%10');
+print "\n";
+$s = 'aa & &amp; a=%10';
+print $s."\n";
+//print dol_htmlentitiesbr($s)."\n";
+//print dol_escape_htmltag(dol_string_onlythesehtmltags(dol_htmlentitiesbr($s), 1, 0, 0, 0, array('br', 'b', 'font', 'hr', 'span')), 1, -1, '', 0, 1);
+print dolPrintHTMLForAttributeUrl('aa & &amp; a=%10');
+print "\n";
+
 
 print $langs->tr("Preview");
 print "\n";
 print $langs->trans("Preview");
 print "\n";
 
-print ">>> dol_escape_htmltag(eée < > bb<b>bold ç) - should not happen</b>\n";
-print dol_escape_htmltag("eée < > bb<b>bold</b> ç", 1);
+print ">>> dol_escape_htmltag(< > bb<b>bold ç &) - should not happen</b>\n";
+print dol_escape_htmltag("< > bb<b>bold</b> ç &", 1);
 print "\n";
-print ">>> dol_escape_htmltag(eée &lt; &gt; bb<b>bold ç)</b>\n";
-print dol_escape_htmltag("eée &lt; &gt; bb<b>bold</b> ç", 1);
+print ">>> dol_escape_htmltag(&lt; &gt; bb<b>bold ç &)</b>\n";
+print dol_escape_htmltag("&lt; &gt; bb<b>bold</b> ç &", 1);
 print "\n";
 print '>>> dol_escape_htmltag(&lt;script&gt;alert("azerty")&lt;/script&gt;)'."\n";
 print dol_escape_htmltag('&lt;script&gt;alert("azerty")&lt;/script&gt;', 1);
@@ -32,11 +44,11 @@ print "\n";
 print "\n";
 
 // dol_escape_htmltag(dol_htmlwithnojs(dol_string_onlythesehtmltags(dol_htmlentitiesbr($s), 1, 1, 1, array())), 1, 1, 'common', 0, 1);
-print ">>> dolPrintHtml(eée < > bb<b>bold</b> ç) - should not happen\n";
-print dolPrintHtml("eée < > bb<b>bold</b> ç");
+print ">>> dolPrintHtml(< > bb<b>bold</b> ç &) - should not happen\n";
+print dolPrintHtml("< > bb<b>bold</b> ç &");
 print "\n";
-print ">>> dolPrintHtml(eée &lt; &gt; bb<b>bold</b> ç)\n";
-print dolPrintHtml("eée &lt; &gt; bb<b>bold</b> ç");
+print ">>> dolPrintHtml(&lt; &gt; bb<b>bold</b> ç &)\n";
+print dolPrintHtml("&lt; &gt; bb<b>bold</b> ç &");
 print "\n";
 print '>>> dolPrintHtml(&lt;script&gt;alert("azerty")&lt;/script&gt;)'."\n";
 print dolPrintHtml('&lt;script&gt;alert("azerty")&lt;/script&gt;');
@@ -45,11 +57,11 @@ print "\n";
 print "\n";
 
 // dol_escape_htmltag(dol_string_onlythesehtmltags(dol_htmlentitiesbr($s), 1, 0, 0, 0, array('br', 'b', 'font', 'hr', 'span')), 1, -1, '', 0, 1);
-print ">>> dolPrintHtmlForattribute(eée < > bb<b>bold</b> ç)\n";
-print dolPrintHTMLForAttribute("eée < > bb<b>bold</b> ç");
+print ">>> dolPrintHtmlForattribute(< > bb<b>bold</b> ç & )\n";
+print dolPrintHTMLForAttribute("< > bb<b>bold</b> ç &");
 print "\n";
-print ">>> dolPrintHTMLForAttribute(eée &lt; &gt; bb<b>bold</b> ç)\n";
-print dolPrintHTMLForAttribute("eée &lt; &gt; bb<b>bold</b> ç");
+print ">>> dolPrintHTMLForAttribute(&lt; &gt; bb<b>bold</b> ç &)\n";
+print dolPrintHTMLForAttribute("&lt; &gt; bb<b>bold</b> ç &");
 print "\n";
 print '>>> dolPrintHtmlForattribute(&lt;script&gt;alert("azerty")&lt;/script&gt;)'."\n";
 print dolPrintHTMLForAttribute('&lt;script&gt;alert("azerty")&lt;/script&gt;');
